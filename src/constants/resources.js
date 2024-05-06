9fcfaee59cd453164d3eb2c161facf1736961658
Piyush Prajapati <piyushoa2004@gmail.com>
2024-05-06 15:31:28 +0530

    basic setup done

---
+++ src/constants/resources.js
@@ -0,0 +1,43 @@
+import IC_ADD from "../res/ic-add.png"
+import IC_ARROW_DOWN_BLUE from "../res/ic-arrow-down-blue.png"
+import IC_ARROW_DOWN from "../res/ic-arrow-down.svg"
+import IC_ARROW_LEFT from "../res/ic-arrow-left.svg"
+import IC_ARROW_RIGHT from "../res/ic-arrow-right.svg"
+import IC_BELL from "../res/ic-bell.png"
+import IC_BUYER_BLUE from "../res/ic-buyer-blue.png"
+import IC_BUYER_WHITE from "../res/ic-buyer-white.png"
+import IC_CAR_BLUE from "../res/ic-car-blue.png"
+import IC_CAR_WHITE from "../res/ic-car-white.png"
+import IC_DASHBOARD_BLUE from "../res/ic-dashboard-blue.png"
+import IC_DASHBOARD_WHITE from "../res/ic-dashboard-white.png"
+import IC_DELETE from "../res/ic-delete.png"
+import IC_EDIT_PEN from "../res/ic-edit-pen.svg"
+import IC_EXCEL from "../res/ic-excel.png"
+import IC_LOGOUT from "../res/ic-logout.png"
+import IC_SEARCH from "../res/ic-search.png"
+import IC_SELLER_BLUE from "../res/ic-seller-blue.png"
+import IC_SELLER_WHITE from "../res/ic-seller-white.png"
+import IMG_BRAND_LOGO from "../res/img-brand-logo.png"
+
+export const R = {
+   ic_add: IC_ADD,
+   ic_arrow_down_blue: IC_ARROW_DOWN_BLUE,
+   ic_arrow_down: IC_ARROW_DOWN,
+   ic_arrow_left: IC_ARROW_LEFT,
+   ic_arrow_right: IC_ARROW_RIGHT,
+   ic_bell: IC_BELL,
+   ic_buyer_blue: IC_BUYER_BLUE,
+   ic_buyer_white: IC_BUYER_WHITE,
+   ic_car_blue: IC_CAR_BLUE,
+   ic_car_white: IC_CAR_WHITE,
+   ic_dashboard_blue: IC_DASHBOARD_BLUE,
+   ic_dashboard_white: IC_DASHBOARD_WHITE,
+   ic_delete: IC_DELETE,
+   ic_edit_pen: IC_EDIT_PEN,
+   ic_excel: IC_EXCEL,
+   ic_logout: IC_LOGOUT,
+   ic_search: IC_SEARCH,
+   ic_seller_blue: IC_SELLER_BLUE,
+   ic_seller_white: IC_SELLER_WHITE,
+   img_brand_logo: IMG_BRAND_LOGO
+}
